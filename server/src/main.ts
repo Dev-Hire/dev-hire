069f34b5d9ae0b717581f7b8c82a6fe7c3f92d90
Brian Taesung Lee <blee421@gatech.edu>
2023-08-06 15:56:49 +0900

    ValidationPipe 추가, class-transformer 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors(); // CORS 허용
 
   const port = configService.get<number>('PORT', 4000); // .env 파일에 PORT가 없으면 기본값으로 4000 사용
