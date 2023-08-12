e753f7a182fea4244d90aecdfa4c4b061a3978f7
te-ing <hi2177@naver.com>
2023-08-13 01:01:42 +0900

    front/feat: 공유버튼 클릭 시 링크 복사 기능 구현

--- client/src/components/recruits/RecruitCard.tsx
+++ client/src/components/recruits/RecruitCard.tsx
@@ -12,33 +12,44 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import { Link } from 'react-router-dom';
+import Toast from '../common/modal/Toast';
+import { useState } from 'react';
 
 export default function RecruitCard({ recruit }: { recruit: Recruit }) {
+  const [isCopy, setIsCopy] = useState(false);
+  const { description, title, user, id, images, startDate, endDate } = recruit;
+
+  const onClickShare = () => {
+    navigator.clipboard.writeText(`${window.location.origin}/recruit-detail/${recruit.id}`);
+    setIsCopy(true);
+  };
+
   return (
     <Card className="recruit-card" sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" height="194" image={recruit.images?.[0]} alt="Paella dish" />
+      <CardMedia component="img" height="194" image={images?.[0]} alt="Paella dish" loading="lazy" />
 
       <CardHeader
-        avatar={<Avatar sx={{ bgcolor: red[500] }}>{getUserName(recruit.user)}</Avatar>}
-        title={recruit.title}
-        subheader={`${recruit.startDate} ~ ${recruit.endDate}`}
+        avatar={<Avatar sx={{ bgcolor: red[500] }}>{getUserName(user)}</Avatar>}
+        title={title}
+        subheader={`${startDate} ~ ${endDate}`}
       />
 
       <CardContent>
         <Typography className="recruit-card-description" variant="body2" color="text.secondary">
-          {recruit.description}
+          {description}
         </Typography>
       </CardContent>
 
       <CardActions className="recruit-card-actions">
-        <IconButton>
+        <IconButton onClick={onClickShare}>
           <ShareIcon />
         </IconButton>
 
-        <Link to={`/recruit-detail/${recruit.id}`}>
+        <Link to={`/recruit-detail/${id}`}>
           <Button>자세히보기</Button>
         </Link>
       </CardActions>
+      {isCopy && <Toast message={`${title}지원 링크가 복사되었습니다.`} onClose={() => setIsCopy(false)} />}
     </Card>
   );
 }
