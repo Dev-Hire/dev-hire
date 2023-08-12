f8f07192a5cb76482c5abb475cf2f53b2b284666
dev <caruen@hanmail.net>
2023-08-12 22:41:16 +0900

    feat: 인증 경로 추가

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticationService } from './authentication/authentication.service';
+import { LocalAuthGuard } from './authentication/local-auth.guard';
+import { JwtAuthGuard } from './authentication/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authenticationService: AuthenticationService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authenticationService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
