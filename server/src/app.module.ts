694cde1e953f4440dbe8b35bc8a0e8009e239800
dev <caruen@hanmail.net>
2023-08-12 22:42:44 +0900

    feat: 인증 모듈 등록

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { RecruitsModule } from './recruiment/recruitment.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecruitsModule } from './recruiment/recruitment.module';
 
 @Module({
   imports: [
@@ -16,12 +18,12 @@ import { RecruitsModule } from './recruiment/recruitment.module';
       imports: [ConfigModule, RecruitsModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URI')
-      })
-    })
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthenticationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-
-export class AppModule { }
+export class AppModule {}
