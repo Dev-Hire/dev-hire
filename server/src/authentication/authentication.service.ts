34b90f0c549243c0aa6c7677f626b202b34df234
dev <caruen@hanmail.net>
2023-08-12 22:39:21 +0900

    feat: 인증 서비스 추가

---
+++ server/src/authentication/authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    // TODO users 모듈에서 findOne구현되면 하드 코딩 삭제
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
